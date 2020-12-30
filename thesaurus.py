e1511af365158b2bccda9b613c82b4423b53df9b
Daniel Lessenden <dless27@gmail.com>
2020-12-30 15:57:07 -0600

    Add complexity to the user interface

--- thesaurus.py
+++ thesaurus.py
@@ -1,4 +1,7 @@
 import json 
+from difflib import SequenceMatcher
+from difflib import get_close_matches
+
 
 data = json.load(open("data.json"))
 
@@ -6,8 +9,18 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        response = input(f"Did you mean {get_close_matches(word, data.keys())[0]} instead of'{word}'? Enter Y for yes or N for no: ")
+        response = response.lower()
+        if response == "y":
+            return data[get_close_matches(word, data.keys())[0]]   
+        elif response == "n":
+            message = f"Sorry, '{word}' does not appear in our dictionary."
+            return print(message)
+        else:
+            return "Sorry, we didn't understand your entry"
     else:
-        message = f"'{word}' does not appear in our dictionary"
+        message = f"Sorry, '{word}' does not appear in our dictionary."
         return print(message)
 
 word = input("Enter word: ")
